a18f901de72f75cfb7725ef61b68c370ed4416b3
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-01-23 15:23:15 +0100

    additional comments

--- queries.sql
+++ queries.sql
@@ -5,7 +5,8 @@
 -- Index: 300183
 
 -------------------------------------------
--- Query #1: id and name of equipment that has never been rented to employees
+-- Query #1: Id and name of equipment that has never been rented to employees 
+-- (equipment id does not exist in equipment_rental table)
 SELECT e.id, e.name 
 FROM equipment e
 LEFT JOIN equipment_rental er
@@ -14,13 +15,13 @@ WHERE er.equipment IS NULL
 ORDER BY e.id ASC;
 
 -------------------------------------------
--- Query #2: employees that have the biggest earnings per hour
+-- Query #2: employees name and surname (concatenated) that have the biggest earnings per hour
 SELECT name || ' ' || surname AS employee, earnings_per_hour 
 FROM employees
 WHERE earnings_per_hour = (SELECT MAX(earnings_per_hour) FROM employees);
 
 -------------------------------------------
--- Query #3: employees with numbers of contracts that they took place in
+-- Query #3: employees with numbers of contracts that they took part
 SELECT e.name , e.surname, COUNT(lc.employee) AS nr_of_contracts
 FROM employees e
 INNER JOIN lease_contracts lc
@@ -43,10 +44,10 @@ INNER JOIN reviewers_dictionary rd
 ORDER BY lc.id ASC;
 
 -------------------------------------------
--- Query #6: nr of archival and non-archival employees
+-- Query #6: number of archival and non-archival employees
 SELECT is_archival, COUNT(is_archival)
 FROM employees
-GROUP BY is_archival
+GROUP BY is_archival;
 
 -------------------------------------------
 -- Query #7: employees that started working in 2016 or 2017
@@ -61,7 +62,7 @@ FROM employees
 WHERE EXTRACT(year FROM date_begin) IN (2016, 2017);
 
 -------------------------------------------
--- Query #8: contracts that length is smaller than 1000 days
+-- Query #8: contracts that length (time between begining and ending) is smaller than 1000 days
 SELECT id, date_begin, date_end, date_end - date_begin AS length 
 FROM lease_contracts
 GROUP BY id, date_begin, date_end
@@ -69,17 +70,19 @@ HAVING date_end - date_begin < 1000;
 
 -------------------------------------------
 -- Query #9: clients that do not take part in any leasing contracts
+-- (client id does not exist in lease_contract)
 SELECT c.id, c.name 
 FROM lease_contracts lc
 RIGHT JOIN clients c
   ON lc.client = c.id
-WHERE lc.client IS NULL
+WHERE lc.client IS NULL;
 
 -------------------------------------------
--- Query #10: the post popular programming langauges used by employees
+-- Query #10: programming languages with number of employees that use them 
+-- (from most popular)
 SELECT ld.name, COUNT(e.programming_language)
 FROM languages_dictionary ld
 INNER JOIN employees e
   ON ld.id = e.programming_language
 GROUP BY ld.name
-ORDER BY COUNT(e.programming_language) DESC
+ORDER BY COUNT(e.programming_language) DESC;
