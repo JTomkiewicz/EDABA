852cff6dd7a0026a65799db5753035b283a474cf
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-01-25 13:58:35 +0100

    nearly finished second trigger

--- triggers.sql
+++ triggers.sql
@@ -35,6 +35,44 @@ WHERE id IN (1, 2, 3);
 
 ------------------------------------------
 -- Trigger #2 INSERT
+-- When new employee is added, equipment (if there is piece not used at the moment) is being rented for him automaticaly 
+
+-- create trigger
+CREATE OR REPLACE TRIGGER give_new_employee_equipment
+AFTER INSERT ON employees
+FOR EACH ROW
+BEGIN
+END;
+
+-- newest equipment rentals (id DESC)
+SELECT id, equipment, employee, date_begin, date_end, date_of_actual_return
+FROM equipment_rental
+ORDER BY id DESC;
+
+-- insert new employee into employees
+INSERT INTO employees VALUES (
+  46,
+  'Jakub',
+  'Tomkiewicz',
+  123456,
+  654321,
+  'testemail@email.com',
+  098765,
+  'Street',
+  150,
+  'Warsaw',
+  'Poland',
+  '01-234',
+  1,
+  10,
+  TO_DATE('2022-01-25'),
+  0
+);
+
+-- newest equipment rentals (id DESC)
+SELECT id, equipment, employee, date_begin, date_end, date_of_actual_return
+FROM equipment_rental
+ORDER BY id DESC;
 
 ------------------------------------------
 -- Trigger #3 DELETE
